a1ccb0da1acdb7ab33cf095bd8fa2f5f67f8cbf1
khadidiatou <khadidiatou.marega@epitech.eu>
2021-12-21 16:42:20 +0100

    add irc

--- client.js
+++ client.js
@@ -1,5 +1,20 @@
-const io = require('socket.io-client')
-const socket = io.connect("ws://localhost:3000");
- io.on("msg", () =>{
-    console.log("Vous avez un nouveau message");
- })
+var socket = require('socket.io-client')('http://localhost:3000');
+  const repl = require('repl')
+  const chalk = require('chalk');
+
+  socket.on('connect', () => {
+      console.log(chalk.red('=== start chatting ==='));
+      username = process.argv[2]
+  });
+  socket.on('disconnect', function() {
+    socket.emit('disconnect')
+});
+  socket.on('message', (data) => {
+    console.log(data);
+  });
+  repl.start({
+      prompt: '',
+      eval: (cmd) => {
+          socket.send(cmd);
+      }
+  })
--- server.js
+++ server.js
@@ -1,10 +1,53 @@
-const app = require('express')();
-const server = require('http').createServer(app);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-const io = require('socket.io')(server);
+ app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-io.on("connexion", () => {
-    console.log("un nouveau est arrivé")
+  io.on('connection', (socket) => {
+        //     socket.on('message', (msg) => {
+        //       console.log("Nouveau message : " + msg);
+        //         socket.broadcast.emit("message", msg);
+        //     });
+        // console.log('Un nouveau utilisateur est arrivé');
+        // socket.on('disconnect', () => {
+        //   console.log('Un utilisateur est parti');
+        // });
+        socket.on('join', ({ name, room }, callback) => {
+ 
+          const { error, user } = addUser(
+              { id: socket.id, name, room });
+   
+          if (error) return callback(error);
+   
+          // Emit will send message to the user
+          // who had joined
+          socket.emit('message', { user: 'admin', text:
+              `${user.name},
+              welcome to room ${user.room}.` });
+   
+          // Broadcast will send message to everyone
+          // in the room except the joined user
+          socket.broadcast.to(user.room)
+              .emit('message', { user: "admin",
+              text: `${user.name}, has joined` });
+   
+          socket.join(user.room);
+   
+          io.to(user.room).emit('roomData', {
+              room: user.room,
+              users: getUsersInRoom(user.room)
+          });
+          callback();
+      });
+
+server.listen(3000, () => {
+  console.log('Le server est lancé sur le port 3000');
 });
 
-server.listen("3000", () => console.log("en attente"));
+// https://socket.io/get-started/private-messaging-part-1/
