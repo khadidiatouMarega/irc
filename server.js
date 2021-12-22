a60ebb10df385ed5a665d0e16d5a081bd18ae8d1
khadidiatou <khadidiatou.marega@epitech.eu>
2021-12-22 12:26:33 +0100

    add irc firs tchat

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
- app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-
-  io.on('connection', (socket) => {
-        //     socket.on('message', (msg) => {
-        //       console.log("Nouveau message : " + msg);
-        //         socket.broadcast.emit("message", msg);
-        //     });
-        // console.log('Un nouveau utilisateur est arrivé');
-        // socket.on('disconnect', () => {
-        //   console.log('Un utilisateur est parti');
-        // });
-        
-      });
-
-server.listen(3000, () => {
-  console.log('Le server est lancé sur le port 3000');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", (socket) => {
+  console.log("Un nouveau utilisateur est arrivé");
+
+  socket.on("user", (username) => {
+    socket.on("message", (data) => {
+      socket.broadcast.emit("message", username + ' : ' + data);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Un utilisateur est parti");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Le server est lancé sur le port 3000");
+});
+
+// https://www.scaleway.com/en/docs/tutorials/socket-io/
+// https://sodocumentation.net/socket-io/topic/9837/handling-users-with-socket-io
 // https://socket.io/get-started/private-messaging-part-1/
