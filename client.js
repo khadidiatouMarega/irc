19d4e09ca733aa2260b6f5e120857bea2c46ef44
khadidiatou <khadidiatou.marega@epitech.eu>
2021-12-22 12:27:02 +0100

    add irc firs tchat

--- client.js
+++ client.js
@@ -1,20 +1,24 @@
-var socket = require('socket.io-client')('http://localhost:3000');
-  const repl = require('repl')
-  const chalk = require('chalk');
+var socket = require("socket.io-client")("http://localhost:3000");
+const repl = require("repl");
+const chalk = require("chalk");
+var username = null;
 
-  socket.on('connect', () => {
-      console.log(chalk.red('=== start chatting ==='));
-      username = process.argv[2]
-  });
-  socket.on('disconnect', function() {
-    socket.emit('disconnect')
+
+socket.on("connect", () => {
+  username = process.argv[2]
+  console.log(chalk.red("=== start chatting ==="));
+  socket.emit("user", username);
 });
-  socket.on('message', (data) => {
-    console.log(data);
-  });
-  repl.start({
-      prompt: '',
-      eval: (cmd) => {
-          socket.send(cmd);
-      }
-  })
+socket.on("disconnect", function () {
+  socket.emit("disconnect");
+});
+socket.on("message", function(data) {
+      console.log(chalk.green(data));
+});
+
+repl.start({
+  prompt: "",
+  eval: (cmd) => {
+    socket.send(cmd);
+  },
+});
